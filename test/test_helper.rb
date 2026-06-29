e58e9f80c52a0d6c213cdaf47c0f6f9b02b65190
Leonel Muñoz <leonel.munoz@gcabsa.com.mx>
2026-06-29 10:02:53 -0700

    First commit
    Signed-off-by: Leonel Muñoz <leonel.munoz@gcabsa.com.mx>

---
+++ test/test_helper.rb
@@ -0,0 +1,30 @@
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+require "minitest/reporters"
+require "factory_bot_rails"
+require "shoulda/matchers"
+
+# Output más legible en consola
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+module ActiveSupport
+  class TestCase
+    # Run tests in parallel with specified workers
+    parallelize(workers: :number_of_processors)
+
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    fixtures :all
+
+    # FactoryBot helpers disponibles en todos los tests
+    include FactoryBot::Syntax::Methods
+  end
+end
+
+# Shoulda Matchers
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :minitest
+    with.library :rails
+  end
+end
